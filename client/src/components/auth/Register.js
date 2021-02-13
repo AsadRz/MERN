442020a799f65d1f2ec80f543a0d6864261a3967
asad <asadullahriaz7@gmail.com>
2021-02-14 01:39:21 +0500

    Login Form added too and removed code for posting users data

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from 'react';
-import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
   const [formData, setFormData] = useState({
@@ -19,28 +19,7 @@ const Register = () => {
     if (password !== password2) {
       console.log('Password Do not match');
     } else {
-      const newUser = {
-        name,
-        email,
-        password,
-      };
-
-      try {
-        const config = {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        };
-        const body = JSON.stringify(newUser);
-        const res = await axios.post(
-          'http://localhost:5000/api/users',
-          body,
-          config
-        );
-        console.log(res.data);
-      } catch (error) {
-        console.log(error.response.data);
-      }
+      console.log('Success');
     }
   };
 
@@ -101,7 +80,7 @@ const Register = () => {
         <input type='submit' className='btn btn-primary' value='Register' />
       </form>
       <p className='my-1'>
-        Already have an account? <a href='login.html'>Sign In</a>
+        Already have an account? <Link to='/login'>Sign In</Link>
       </p>
     </Fragment>
   );
